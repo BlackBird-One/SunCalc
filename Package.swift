74212d015b80165870022180e3bd11001ef87b55
Michal Kus <2879508+BlackBird-One@users.noreply.github.com>
2025-12-14 19:54:42 +0100

    Verze os a přepočty na degrees

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SunCalc",
     defaultLocalization: "en",
-	platforms: [.iOS(.v16), .macCatalyst(.v16), .macOS(.v13)],
+	platforms: [.iOS(.v17), .macCatalyst(.v17), .macOS(.v14), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
--- Sources/SunCalc/models/SunPosition.swift
+++ Sources/SunCalc/models/SunPosition.swift
@@ -15,4 +15,22 @@ public class SunPosition {
 		self.azimuth = azimuth
 		self.altitude = altitude
 	}
+
+    public var altitudeDegrees: Double {
+        altitude * 180.0 / .pi
+    }
+
+    public var azimuthDegrees: Double {
+        180.0 + azimuth * 180.0 / .pi
+    }
+
+    /// Normalizuje azimut na rozsah 0-360°
+    public var azimuthDegreesNormalized: Double {
+        var normalized = 180.0 + azimuth * 180.0 / .pi
+        normalized = normalized.truncatingRemainder(dividingBy: 360.0)
+        if normalized < 0 {
+            normalized += 360.0
+        }
+        return normalized
+    }
 }
